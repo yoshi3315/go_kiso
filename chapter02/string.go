85934a96ac04a164157aa3a9e6748e9dc561c6ca
yoshiki chiba <yoshiki.chiba@one-star.jp>
2019-12-29 11:52:48 +0900

    section7:型について

---
+++ chapter02/string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var str string
+
+	str = "あ"
+
+	str = str + "い"
+
+	str += "う"
+
+	fmt.Println(str)
+}
---
+++ chapter02/string_count.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+
+	var ja string = "Go言語"
+
+	fmt.Println(ja, " len:", utf8.RuneCountInString(ja))
+}
