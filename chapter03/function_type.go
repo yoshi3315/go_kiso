d969bbdda7861c2d1ec9dffaad6d77082ea15c88
yoshiki chiba <yoshiki.chiba@one-star.jp>
2019-12-31 11:59:09 +0900

    section21:関数

---
+++ chapter03/function_type.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var f func(int, int) int
+
+	f = func(a int, b int) int {
+		return a + b
+	}
+
+	fmt.Println(f(1, 2))
+
+	f = multiply
+
+	fmt.Println(f(1, 2))
+}
+
+func multiply(x int, y int) int {
+	return x * y
+}
