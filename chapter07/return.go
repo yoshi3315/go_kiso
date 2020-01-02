2acec4eaa0f7ee981351736786817fffe6d87d06
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-01-02 22:36:09 +0900

    section36:戻り値によるエラーハンドリング

---
+++ chapter07/return.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("test.txt")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	file.Close()
+
+	fmt.Println("OK")
+}
